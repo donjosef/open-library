f0e3f95c53acc23387b3d22d2b48ed7fcbf24ce6
donjoe <giumontanaro@gmail.com>
2020-12-16 15:53:52 +0100

    Render different navigation based on media

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, { useState } from 'react'
+import React from 'react'
 import {LibraryProvider} from './contexts/libraryContext'
 import LibraryPage from './pages/LibraryPage/LibraryPage'
 import ExplorePage from './pages/ExplorePage/ExplorePage'
 import FullBook from './components/FullBook/FullBook'
 import Navigation from './components/Navigation/Navigation'
+import NavigationTablet from './components/Navigation/NavigationTablet'
+import NavigationMobile from './components/Navigation/NavigationMobile'
+import { useMedia } from './hooks/useMedia'
 import {Route, Redirect} from 'react-router-dom'
 import './App.css';
 
 function App() {
+  const media = useMedia()
 
   return (
     <div className="app">
-      <Navigation />
+      {
+      media === 'desktop' ? <Navigation /> :
+      media === 'tablet' ? <NavigationTablet /> : 
+      <NavigationMobile />
+      }
       <LibraryProvider>
         <Route path="/explore" component={ExplorePage} />
         <Route path="/library" component={LibraryPage} />
