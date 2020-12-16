ec7f49b953c659454190e30202210b29b3781266
donjoe <giumontanaro@gmail.com>
2020-12-16 15:56:19 +0100

    Use NavigationLinks component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import SearchBar from '../SearchBar/SearchBar'
-import {NavLink} from 'react-router-dom'
+import NavigationLinks from './NavigationLinks/NavigationLinks'
 import './Navigation.css'
 
 function Navigation(props) {
     return (
         <nav className="navigation">
             <div className="navigation__logo"></div>
-            <div className="navigation__links">
-                <NavLink to="/explore">Explore</NavLink>
-                <NavLink to="/categories">Categories</NavLink>
-                <NavLink to="/library">Library</NavLink>
-            </div>
+            <NavigationLinks />
             <SearchBar />
         </nav>
     )
