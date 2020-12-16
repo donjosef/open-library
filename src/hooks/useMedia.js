4b53a05ca360da8f755665faa6ce29a1d78b20e8
donjoe <giumontanaro@gmail.com>
2020-12-16 15:52:25 +0100

    custom hook useMedia for responsivness

---
+++ src/hooks/useMedia.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+
+export const useMedia = () => {
+    const [media, setMedia] = useState('')
+
+    useEffect(() => {
+        getWidth()
+        window.addEventListener('resize', handleResize)
+    }, [])
+
+    const getWidth = () => { 
+        const width = window.innerWidth
+        if(width > 761) {
+            setMedia('desktop')
+        }
+
+        if(width > 450 && width < 760) {
+            setMedia('tablet')
+        }
+
+        if(width < 450) {
+            setMedia('mobile')
+        }
+    }
+
+    const handleResize = () => {
+        getWidth()
+    }
+
+    return media
+}
