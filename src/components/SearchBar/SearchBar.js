a7f798bebaf4f0895faeaeb06e81f16020b182e1
donjoe <giumontanaro@gmail.com>
2020-12-16 18:17:43 +0100

    Improve searchbar results avoid unpredictability

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react'
 import SearchResults from '../SearchResults/SearchResults'
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import './SearchBar.css'
 
 function SearchBar(props) {
@@ -10,16 +10,17 @@ function SearchBar(props) {
     const [error, setError] = useState(null)
     const [isBookSelected, setIsBookSelected] = useState(false)
     const timeoutId = useRef()
+    const bookValueRef = useRef()
     const history = useHistory()
 
-
     useEffect(() => { //after input value is set
+        bookValueRef.current = bookValue
         if (!isBookSelected) { //avoid to getBooks when selecting from searchResults
             clearTimeout(timeoutId.current)
             if (bookValue.length > 1) {
                 timeoutId.current = setTimeout(() => { //debounce
                     getBooks()
-                }, 500)
+                }, 100)
             } else {
                 setBooks([])
             }
@@ -40,26 +41,41 @@ function SearchBar(props) {
             .then(res => res.json())
             .then(data => {
                 if (data.docs.length) {
-                    const books = data.docs
-                        .filter(doc => doc.isbn || doc.lccn) //filter out doc without isbn or lccn. Will be important when user select a book from searchResults. In fact, if user would select a book without an isbn or lccn the response of handleSelectBook will be an empty object
-                        .map(({ title, publisher, publish_year, author_name, key, isbn, lccn }) => {
-                            return {
-                                isbn: isbn ? isbn[0] : undefined,
-                                lccn: lccn ? lccn[0] : undefined,
-                                key: key,
-                                title: title || 'unknown',
-                                publisher: Array.isArray(publisher) ? publisher[0] : 'unknown',
-                                publish_year: Array.isArray(publish_year) ? publish_year[0] : 'unknown',
-                                author_name: Array.isArray(author_name) ? author_name[0] : 'unknown'
-                            }
-                        })
-                    /* 
-                    no state batching inside promise. There will be 2 different render
-                    https://stackoverflow.com/questions/53048495/does-react-batch-state-update-functions-when-using-hooks
+                    /*
+                    SIMPLE EXPLANATION
+                    The problem was that fetch could resolve unpredictably.
+                    Say we have 3 call to getBooks()
+                    The last could resolve BEFORE the first
+                    So in order to avoid to reSet the state i make certain to check the books returned with the current (the latest) value of bookValue (checking title and author against a regular expression built with the latest value of bookValue)
+
+                    Example: if getBooks() invoked with "tol" as bookValue resolves when the input is already "tolkien", and books of SearchResults are already "tolkien", is a bad user experience to reSet the state books with "tol", changing SearchResults
+
+                    WHY am i using a ref instead of bookValue of state? 
+                    Because when getBooks gets invoked it has a closure(that's important!) to the current (old) bookValue
                     */
-                    setBooks(books)
-                    setBooksLoading(false)
-                    setError(null)
+                    const regEx = new RegExp(bookValueRef.current, 'i')
+                    if(data.docs.some(doc => regEx.test(doc.title) || regEx.test(doc.author_name) )) {
+                        const books = data.docs
+                            .filter(doc => doc.isbn || doc.lccn) //filter out doc without isbn or lccn. Will be important when user select a book from searchResults. In fact, if user would select a book without an isbn or lccn the response of handleSelectBook will be an empty object
+                            .map(({ title, publisher, publish_year, author_name, key, isbn, lccn }) => {
+                                return {
+                                    isbn: isbn ? isbn[0] : undefined,
+                                    lccn: lccn ? lccn[0] : undefined,
+                                    key: key,
+                                    title: title || 'unknown',
+                                    publisher: Array.isArray(publisher) ? publisher[0] : 'unknown',
+                                    publish_year: Array.isArray(publish_year) ? publish_year[0] : 'unknown',
+                                    author_name: Array.isArray(author_name) ? author_name[0] : 'unknown'
+                                }
+                            })
+                        /* 
+                        no state batching inside promise. There will be 3 different render
+                        https://stackoverflow.com/questions/53048495/does-react-batch-state-update-functions-when-using-hooks
+                        */
+                        setBooks(books)
+                        setBooksLoading(false)
+                        setError(null)
+                    }
                 } else {
                     const error = new Error('No books found. Be more specific.')
                     throw error
