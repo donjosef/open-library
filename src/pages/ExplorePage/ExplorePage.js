dc7665858989da30f5484294ecc6acaccd792809
donjoe <giumontanaro@gmail.com>
2020-12-18 15:09:30 +0100

    Render Slider populating it with response of subjects

--- src/pages/ExplorePage/ExplorePage.js
+++ src/pages/ExplorePage/ExplorePage.js
@@ -1,108 +1,87 @@
-import React, { useState, useEffect, useRef} from 'react'
-import SearchResults from '../../components/SearchResults/SearchResults'
-import FullBook from '../../components/FullBook/FullBook'
+import React, { useState, useEffect } from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+
 import './ExplorePage.css'
 
 function ExplorePage(props) {
-    const [books, setBooks] = useState([])
-    const [booksLoading, setBooksLoading] = useState(false)
-    const [error, setError] = useState(null)
-    const [bookValue, setBookValue] = useState('')
-    const [isBookSelected, setIsBookSelected] = useState(false)
-    const [fullBook, setFullBook] = useState(null)
-    const timeoutId = useRef()
+    const [categories, setCategories] = useState({})
 
-    useEffect(() => { //after input value is set
-        if (!isBookSelected) { //avoid to getBooks when selecting from searchResults
-            clearTimeout(timeoutId.current)
-            if (bookValue.length > 1) {
-                timeoutId.current = setTimeout(() => { //debounce
-                    getBooks()
-                }, 500)
+    useEffect(() => {
+        const categories = ['arts', 'fiction', 'sciencemathematics', 'business', 'juvenile_fiction', 'history', 'health', 'biography', 'social_sciences', 'textbooks']
+        let randoms = []
+        for (let i = 0; i < 3; i++) {
+            const random = Math.floor(Math.random() * categories.length)
+            if (!randoms.includes(random)) {
+                randoms[i] = random
             } else {
-                setBooks([])
+                i--
             }
         }
 
-    }, [bookValue])
+        randoms.forEach(random => getCategory(categories[random]))
+    }, [])
 
-    const handleBookValue = (value) => {
-        setBookValue(value)
-        setIsBookSelected(false) //otherwise it will not getBooks anymore (in fact after being selected is true. See useEffect conditional)
-    }
+    const getCategory = (category) => {
 
-    const getBooks = () => {
-        const formattedBook = bookValue.trim().replace(/\s/g, "+")
-
-        setBooksLoading(true)
-        fetch(`https://openlibrary.org/search.json?q=${formattedBook}&limit=10`)
+        fetch(`https://openlibrary.org/subjects/${category}.json`)
             .then(res => res.json())
             .then(data => {
-                if (data.docs.length) {
-                    const books = data.docs
-                        .filter(doc => doc.isbn || doc.lccn) //filter out doc without isbn or lccn. Will be important when user select a book from searchResults. In fact, if user would select a book without an isbn or lccn the response of handleSelectBook will be an empty object
-                        .map(({ title, publisher, publish_year, author_name, key, isbn, lccn }) => {
-                            return {
-                                isbn: isbn ? isbn[0] : undefined,
-                                lccn: lccn ? lccn[0] : undefined,
-                                key: key,
-                                title: title || 'unknown',
-                                publisher: Array.isArray(publisher) ? publisher[0] : 'unknown',
-                                publish_year: Array.isArray(publish_year) ? publish_year[0] : 'unknown',
-                                author_name: Array.isArray(author_name) ? author_name[0] : 'unknown'
-                            }
-                        })
-                    /* 
-                    no state batching inside promise. There will be 2 different render
-                    https://stackoverflow.com/questions/53048495/does-react-batch-state-update-functions-when-using-hooks
-                    */
-                    setBooks(books)
-                    setBooksLoading(false)
-                    setError(null)
-                } else {
-                    const error = new Error('No books found. Be more specific.')
-                    throw error
-                }
-            })
-            .catch(err => {
-                setError(err.message)
-                setBooks([])
+                console.log(data)
+                const works = data.works
+                setCategories(prevCategories => {
+                    return {
+                        ...prevCategories,
+                        [data.name]: works
+                    }
+                })
             })
     }
 
-    const handleSelectBook = (book) => {
-        /*in this case is better to update state BEFORE the promise will resolve. React will BATCH state updates, as opposed to asyncronous block like 'then'
-        */
-        setBookValue(book.title + ' ' + book.author_name)
-        setBooks([])
-        setIsBookSelected(true)
-        const bibKeys = book.isbn ? 'ISBN' : 'LCCN'
-        fetch(`https://openlibrary.org/api/books?bibkeys=${bibKeys}:${book.isbn || book.lccn}&format=json&jscmd=data`)
-            .then(res => res.json())
-            .then(data => {
-                setFullBook(data)
-            })
-            .catch(err => alert(err))
+    const settings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3
     }
+
+    const [firstCategory, secondCategory, thirdCategory] = Object.keys(categories)
+    let src = 'https://covers.openlibrary.org/b/id/'
 
     return (
-        <div className="search-page">
-            <form className="search-page__form">
-                <input
-                    type="search"
-                    className="search-page__input"
-                    value={bookValue}
-                    placeholder="Search"
-                    onChange={(e) => handleBookValue(e.target.value)} />
-                {(books.length > 0 || booksLoading) && (
-                    <SearchResults
-                        books={books}
-                        onSelectBook={handleSelectBook}
-                        loading={booksLoading} />
-                )}
-            </form>
-            {error && <h3>{error}</h3>}
-            {fullBook && <FullBook book={fullBook} />}
+        <div className="explore-page">
+            <div className="explore-page__slider">
+                <h2>{firstCategory}</h2> 
+                <Slider {...settings}>
+                    {categories[firstCategory] && categories[firstCategory].map(book => (
+                    <div key={book.cover_id}>
+                        <img src={src + `${book.cover_id}-M.jpg`} alt={book.title}/>
+                    </div>  
+                    ))}
+                </Slider>
+            </div>
+
+            <div className="explore-page__slider">
+                <h2>{secondCategory}</h2>
+                <Slider {...settings}>
+                    {categories[secondCategory] && categories[secondCategory].map(book => (
+                    <div key={book.cover_id}>
+                        <img src={src + `${book.cover_id}-M.jpg`} alt={book.title}/>
+                    </div>  
+                    ))}
+                </Slider>
+            </div>
+
+            <div className="explore-page__slider">
+                <h2>{thirdCategory}</h2>
+                <Slider {...settings}>
+                    {categories[thirdCategory] && categories[thirdCategory].map(book => (
+                    <div key={book.cover_id}>
+                        <img src={src + `${book.cover_id}-M.jpg`} alt={book.title}/>
+                    </div>  
+                    ))}
+                </Slider>
+            </div>
         </div>
     )
 }
