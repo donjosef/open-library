46131dbe1a4c1a6208087fcfa389596be26fc813
donjoe <giumontanaro@gmail.com>
2020-12-22 15:17:51 +0100

    Handle loading state for slider

--- src/pages/ExplorePage/ExplorePage.js
+++ src/pages/ExplorePage/ExplorePage.js
@@ -4,6 +4,7 @@ import './ExplorePage.css'
 
 function ExplorePage(props) {
     const [categories, setCategories] = useState({})
+    const [loadings, setLoadings] = useState({})
 
     useEffect(() => {
         const categories = ['arts', 'fiction', 'sciencemathematics', 'business', 'juvenile_fiction', 'history', 'health', 'biography', 'social_sciences', 'textbooks']
@@ -21,11 +22,9 @@ function ExplorePage(props) {
     }, [])
 
     const getCategory = (category) => {
-
         fetch(`https://openlibrary.org/subjects/${category}.json`)
             .then(res => res.json())
             .then(data => {
-                console.log(data)
                 const works = data.works.filter(work => work.cover_id)
                 setCategories(prevCategories => {
                     return {
@@ -33,16 +32,33 @@ function ExplorePage(props) {
                         [data.name]: works //will be {category1: [], category2: [], category3: []}
                     }
                 })
+
+                setLoadings(prevLoadings => {
+                    return {
+                        ...prevLoadings,
+                        [data.name]: false
+                    }
+                })
             })
     }
 
     const [firstCategory, secondCategory, thirdCategory] = Object.keys(categories)
+    const [firstLoading = true, secondLoading = true, thirdLoading = true] = Object.values(loadings) //Default values in destructuring assignement only work if the variables either don't exist or their value is set to undefined  
 
     return (
         <div className="explore-page">
-            <ImageSlider category={firstCategory} categories={categories[firstCategory]} />
-            <ImageSlider category={secondCategory} categories={categories[secondCategory]} />
-            <ImageSlider category={thirdCategory} categories={categories[thirdCategory]} />
+            <ImageSlider
+                loading={firstLoading}
+                category={firstCategory}
+                categories={categories[firstCategory]} />
+            <ImageSlider
+                loading={secondLoading}
+                category={secondCategory}
+                categories={categories[secondCategory]} />
+            <ImageSlider
+                loading={thirdLoading}
+                category={thirdCategory}
+                categories={categories[thirdCategory]} />
         </div>
     )
 }
